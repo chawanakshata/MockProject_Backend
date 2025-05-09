cd243186d407af84a2fd229cb99a3519945f3070
chawanakshata <90598127+chawanakshata@users.noreply.github.com>
2025-05-09 11:31:50 +0530

    Delete Mock Project/Models/FileUploadRequest.cs

--- Mock Project/Models/FileUploadRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿//namespace Mock_Project.Models
-//{
-//    public class FileUploadRequest
-//    {
-//        public int Id { get; set; }
-//        public string TeamMemberName { get; set; }
-//        public IFormFile File { get; set; }
-//    }
-//}
